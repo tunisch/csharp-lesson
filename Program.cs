756ac1f3a590a1b4da6edb032e9f69ba259214b1
Tunahan Turker Erturk <89503561+tunisch@users.noreply.github.com>
2026-04-15 16:27:58 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Isminizi Giriniz");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyisiminzi giriniz");
+            string surname = Console.ReadLine();
+            Console.WriteLine(name + " " + surname );
+
+
+        }
+    }
+}
