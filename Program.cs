8ca64761ffd90de2fd13c5f9c8080520afd5cac8
TT <200706019@st.maltepe.edu.tr>
2026-04-27 18:32:18 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Implicitly typed variable Conversion (Bilincsiz Tür Dönüşümü)
+        byte a = 5;
+        sbyte b = 30;
+        short c = 10;
+
+// a, b, c değişkenlerinin toplamını int türünde bir değişkene atayalım kapasitesi daha büyük olduğu için int turune atayabiliriz hata vermez boylelikle.
+        int d = a + b + c;
+        Console.WriteLine("d: " + d); 
+
+
+        // Excplicitly typed variable Conversion (Bilincli Tür Dönüşümü)
+    }
+}
